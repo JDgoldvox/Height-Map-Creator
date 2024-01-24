c59f9eb837a0d2d86f03fe68996598181154dca5
Wang Grundy <2101005@uad.ac.uk>
2024-01-24 13:33:30 -0500

    first attempt at real perlin
    made a scale and a frequency var to make details more seeable

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -6,6 +6,9 @@ public class TerrainGenerator : MonoBehaviour
 {
     float temp = 0;
 
+    private float frequency = 0.1f;
+    private float scale = 10f;
+
     [SerializeField] private WorldSize worldSize = new WorldSize();
 
     [SerializeField] private GameObject grassCube;
@@ -41,11 +44,11 @@ public class TerrainGenerator : MonoBehaviour
         {
             for(int z = 0; z < worldSize.z; z++)
             {
-                float scaledX = x * 0.4f; // Adjust the scale as needed
-                float scaledZ = z * 0.1f; // Adjust the scale as needed
+                float adjustedX = x * frequency; // Adjust the scale as needed
+                float adjustedZ = z * frequency; // Adjust the scale as needed
 
-                noiseNumber = NoiseFunction.GenerateNoise(scaledX, scaledZ);
-                GameObject newCubeObj = Instantiate(grassCube, new Vector3(x, noiseNumber, z), Quaternion.identity);
+                noiseNumber = NoiseFunction.GenerateNoise(adjustedX, adjustedZ);
+                GameObject newCubeObj = Instantiate(grassCube, new Vector3(x, noiseNumber * scale, z), Quaternion.identity);
                 worldCubes.Add(newCubeObj);
 
                 Debug.Log($"created new cube with: {noiseNumber} as y\n");
